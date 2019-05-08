267d5f26dd3a03593110d83fc137cd583409e66b
joseslraimundo <ze_raimundo27@hotmail.com>
2019-05-08 16:40:44 +0100

    Implementação da interface da classe LoginGUI

--- lab/LoginGUI.java
+++ lab/LoginGUI.java
@@ -33,7 +33,64 @@ public class LoginGUI {
 		addLoginDialogContent();
 	}
 	
+	private void addLoginDialogContent() {
 	
+	JPanel loginPanel = new JPanel(new GridBagLayout());
+	JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+	buttonPanel.add(okButton);
+	buttonPanel.add(cancelButton);
+
+	GridBagConstraints gbc = new GridBagConstraints();
+
+	gbc.insets = new Insets(4, 4, 4, 4);
+
+	gbc.gridx = 0;
+	gbc.gridy = 0;
+	gbc.weightx = 0;
+	loginPanel.add(nameLabel, gbc);
+
+	gbc.gridx = 1;
+	gbc.gridy = 0;
+	gbc.weightx = 1;
+	gbc.fill = GridBagConstraints.HORIZONTAL;
+	loginPanel.add(nameField, gbc);
+
+	gbc.gridx = 0;
+	gbc.gridy = 1;
+	gbc.weightx = 0;
+	loginPanel.add(passwordLabel, gbc);
+
+	gbc.gridx = 1;
+	gbc.gridy = 1;
+	gbc.weightx = 1;
+	gbc.fill = GridBagConstraints.HORIZONTAL;
+	loginPanel.add(passwordField, gbc);
+
+	login = new JDialog();
+	login.setPreferredSize(new Dimension(400,300));
+	login.add(loginPanel);
+	login.add(buttonPanel, BorderLayout.SOUTH);
+	login.setVisible(true);
+	Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+	login.setLocation(dimension.width/2 - 400/2, dimension.height/2 - 300/2);
+	login.setTitle("Login");
+	login.pack();
+	login.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+	
+	LoginGUI log = this;
+	
+	okButton.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+		
+
+		
+
+		
+	});
 	
 	cancelButton.addActionListener(new ActionListener() {
 		@Override
@@ -43,10 +100,16 @@ public class LoginGUI {
 	});
 	
 	}
-
+	
+	public char getAccount() {
+		// TODO Auto-generated method stub
+		return 'I';
+	}
+	
 
 	public static void main(String[] args) {
 		new LoginGUI();
 	}
 
 }
+
